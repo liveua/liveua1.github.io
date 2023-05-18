471f602f5287280865550ed0516fd7623ec0ca9f
liveua <89434809+liveua@users.noreply.github.com>
2023-05-18 23:17:41 +0800

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,26 @@
+// music-data.js 文件内容
+var musicData = [{
+        name: 'China-X',
+        artist: '徐梦圆',
+        url: 'https://www.ytmp3.cn/down/49003.mp3',
+        cover: 'https://p1.music.126.net/5zs7IvmLv7KahY3BFzUmrg==/109951163635241613.jpg?param=300y300',
+      },
+      {
+        name: '这是一封离别信',
+        artist: '刘大拿',
+        url: 'http://music.163.com/song/media/outer/url?id=1923184888.mp3',
+        cover: 'http://p1.music.126.net/zmC73kE-LO370J4kjCZZyA==/109951167087160304.jpg?param=130y130',
+      },
+      {
+                title: '半岛铁盒',
+                author: '周杰伦',
+                url: 'https://echeverra.cn/wp-content/uploads/2022/05/周杰伦-半岛铁盒.mp3',
+                pic: 'https://echeverra.cn/wp-content/uploads/2022/05/周杰伦-半岛铁盒-mp3-image.png'
+        	},
+            {
+                title: '给我一首歌的时间',
+                author: '周杰伦',
+                url: 'https://echeverra.cn/wp-content/uploads/2021/06/周杰伦-给我一首歌的时间.mp3',
+                pic: 'https://echeverra.cn/wp-content/uploads/2021/06/周杰伦-给我一首歌的时间-mp3-image.png'
+            }
+		]
